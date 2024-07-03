677008ec829db7a23ed437989b02dee0c7d1238a
Aryaman Somvanshi <aryaman1505@gmail.com>
2024-07-04 00:15:20 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,10 +4,10 @@ import Sidebar from "../../components/sidebar/Sidebar";
 const Home = () => {
     return (
         <div className="flex h-screen overflow-hidden">
-            <div className="w-full md:w-1/3 h-full bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30">
+            <div className="w-full md:w-3/8 h-full bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 p-4">
                 <Sidebar />
             </div>
-            <div className="w-full md:w-2/3 h-full overflow-y-auto bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30">
+            <div className="w-full md:w-5/8 h-full overflow-y-auto bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 p-4">
                 <MessageContainer />
             </div>
         </div>
