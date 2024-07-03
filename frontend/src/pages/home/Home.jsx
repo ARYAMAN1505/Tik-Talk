2147b87205ee8f3802207a62218c62a8b0fd4fe8
Aryaman Somvanshi <aryaman1505@gmail.com>
2024-07-03 23:24:09 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,11 +2,12 @@ import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
-	return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar />
-			<MessageContainer />
-		</div>
-	);
+    return (
+        <div className='flex flex-col md:flex-row items-stretch rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <Sidebar className='w-full md:w-1/4' />
+            <MessageContainer className='w-full md:w-3/4' />
+        </div>
+    );
 };
+
 export default Home;
