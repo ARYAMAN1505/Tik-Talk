0c8260784f3fa8be4e2bec59afa921780a4f30da
Aryaman Somvanshi <aryaman1505@gmail.com>
2024-07-03 23:59:28 +0530

    Update Sidebar.jsx

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
     return (
-        <div className="border-r border-slate-500 p-4 flex flex-col h-full">
+        <div className="border-r border-slate-500 p-4 flex flex-col h-full bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30">
             <SearchInput />
             <div className="divider px-3"></div>
             <Conversations className="flex-grow overflow-y-auto" />
