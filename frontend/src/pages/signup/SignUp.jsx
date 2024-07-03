07ca75c7799b576f9a0e6faab24513bccf407936
Aryaman Somvanshi <aryaman1505@gmail.com>
2024-07-03 23:24:53 +0530

    Update SignUp.jsx

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -24,39 +24,31 @@ const SignUp = () => {
     };
 
     return (
-        <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-            <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+        <div className='flex flex-col items-center justify-center min-h-screen'>
+            <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 md:w-96'>
                 <div className='text-center'>
                     <h1 className='text-3xl font-semibold text-gray-300'>
                         Tik-Talk
                     </h1>
-                         <h1 className='text-xl font-semibold text-gray-300 mb-4'>
-                        Where conersations sync like clockwork
-                    </h1>
-                    <br></br>
-                    <h1 className='text-3xl font-semibold text-gray-300 mb-4'>
-                        Signup
+                    <h1 className='text-xl font-semibold text-gray-300 mb-4'>
+                        Where conversations sync like clockwork
                     </h1>
                 </div>
 
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label className='label p-2'>
-                            <span className='text-base text-white label-text'>Full Name</span>
-                        </label>
+                <form onSubmit={handleSubmit} className='w-full'>
+                    <div className='mb-4'>
+                        <label className='text-base label-text text-white'>Full Name</label>
                         <input
                             type='text'
                             placeholder='Aryaman Somvanshi'
-                            className='w-full input input-bordered  h-10'
+                            className='w-full input input-bordered h-10'
                             value={inputs.fullName}
                             onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
                         />
                     </div>
 
-                    <div>
-                        <label className='label p-2 '>
-                            <span className='text-base text-white label-text'>Username</span>
-                        </label>
+                    <div className='mb-4'>
+                        <label className='text-base label-text text-white'>Username</label>
                         <input
                             type='text'
                             placeholder='Aryaman'
@@ -66,10 +58,8 @@ const SignUp = () => {
                         />
                     </div>
 
-                    <div>
-                        <label className='label'>
-                            <span className='text-base text-white label-text'>Password</span>
-                        </label>
+                    <div className='mb-4'>
+                        <label className='text-base label-text text-white'>Password</label>
                         <input
                             type='password'
                             placeholder='Enter Password'
@@ -79,10 +69,8 @@ const SignUp = () => {
                         />
                     </div>
 
-                    <div>
-                        <label className='label'>
-                            <span className='text-base text-white label-text'>Confirm Password</span>
-                        </label>
+                    <div className='mb-4'>
+                        <label className='text-base label-text text-white'>Confirm Password</label>
                         <input
                             type='password'
                             placeholder='Confirm Password'
@@ -101,12 +89,11 @@ const SignUp = () => {
                         Already have an account?
                     </Link>
 
-                    <div>
-                        <button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
-                            {loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
-                        </button>
-                    </div>
+                    <button className='btn btn-block btn-sm mt-4 border border-slate-700' disabled={loading}>
+                        {loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+                    </button>
                 </form>
+
                 <footer className='text-white text-center mt-4'>
                     Made By Aryaman
                 </footer>
@@ -114,4 +101,5 @@ const SignUp = () => {
         </div>
     );
 };
+
 export default SignUp;
