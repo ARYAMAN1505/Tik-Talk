97868496289c490830e4d830c074944a7a3dd65a
Aryaman Somvanshi <aryaman1505@gmail.com>
2024-07-03 23:45:15 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,9 +3,13 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
     return (
-        <div className='flex flex-col md:flex-row items-stretch rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-            <Sidebar className='w-full md:w-1/4' />
-            <MessageContainer className='w-full md:w-3/4' />
+        <div className='flex flex-col md:flex-row items-stretch h-screen overflow-y-auto rounded-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <div className='w-full md:w-1/4'>
+                <Sidebar />
+            </div>
+            <div className='w-full md:w-3/4'>
+                <MessageContainer />
+            </div>
         </div>
     );
 };
