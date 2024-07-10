eb1043e820ecb75aa2820e957bff750eb4983018
Aryaman Somvanshi <aryaman1505@gmail.com>
2024-07-10 23:37:11 +0530

    Update useGetMessages.js

--- frontend/src/hooks/useGetMessages.js
+++ frontend/src/hooks/useGetMessages.js
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react";
 import useConversation from "../zustand/useConversation";
 import toast from "react-hot-toast";
@@ -11,7 +10,7 @@ const useGetMessages = () => {
 		const getMessages = async () => {
 			setLoading(true);
 			try {
-				const res = await fetch(/api/messages/${selectedConversation._id});
+				const res = await fetch(`/api/messages/${selectedConversation._id}`);
 				const data = await res.json();
 				if (data.error) throw new Error(data.error);
 				setMessages(data);
