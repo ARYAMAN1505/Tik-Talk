aa2ad2d3cac7b3bc03016617867a9708f5996da6
Aryaman Somvanshi <aryaman1505@gmail.com>
2024-07-10 23:20:23 +0530

    Update MessageContainer.jsx

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -14,36 +14,33 @@ const MessageContainer = () => {
     }, [setSelectedConversation]);
 
     return (
-        <div className="md:min-w-[450px] flex flex-col h-full overflow-y-auto bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30">
+        <div className='md:min-w-[450px] flex flex-col'>
             {!selectedConversation ? (
                 <NoChatSelected />
             ) : (
                 <>
                     {/* Header */}
-                    <div className="bg-slate-500 px-4 py-2 mb-2">
-                        <span className="label-text">To:</span>{" "}
-                        <span className="text-gray-900 font-bold">{selectedConversation.fullName}</span>
+                    <div className='bg-slate-500 px-4 py-2 mb-2'>
+                        <span className='label-text'>To:</span>{" "}
+                        <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
                     </div>
-                    <div className="flex-grow overflow-y-auto">
-                        <Messages />
-                    </div>
+                    <Messages />
                     <MessageInput />
                 </>
             )}
         </div>
     );
 };
-
 export default MessageContainer;
 
 const NoChatSelected = () => {
     const { authUser } = useAuthContext();
     return (
-        <div className="flex items-center justify-center w-full h-full">
-            <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+        <div className='flex items-center justify-center w-full h-full'>
+            <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
                 <p>Welcome 👋 {authUser.fullName} ❄</p>
                 <p>Select a chat to start messaging</p>
-                <TiMessages className="text-3xl md:text-6xl text-center" />
+                <TiMessages className='text-3xl md:text-6xl text-center' />
             </div>
         </div>
     );
